ef154b0fde1863249f02b78677e5ef931c8d9f34
blazehp <blazehunterhp@gmail.com>
2026-04-21 20:11:45 +0200

    Connected MYSQL database to website and added queries to create tables

--- app.py
+++ app.py
@@ -3,14 +3,13 @@
 from flask import Flask, render_template, redirect
 import os
 from dotenv import load_dotenv
+from database_config import db
 
 # Load environment variables into the server
 # This is the .env when in development mode
 load_dotenv()
 
 app = Flask(__name__)
-
-DB_URL = os.getenv("DB_URL")
 PORT = os.getenv("PORT")
 
 @app.route('/')
@@ -19,9 +18,86 @@ def index():
 
 @app.route('/blast')
 def blast():
+  print(f"Is connected? {db.is_connected()}")
   return render_template('blast.html')
 
+def delete_all_tables(cur):
+  """
+  DANGER: Only run when need to do a **full wipe**!
+  Delete all tables in the database.
+  """
+  cur.execute("DROP TABLE IF EXISTS input")
+  cur.execute("DROP TABLE IF EXISTS raw_blast")
+  cur.execute("DROP TABLE IF EXISTS protein")
+  cur.execute("DROP TABLE IF EXISTS organism")
+  cur.execute("DROP TABLE IF EXISTS filtered_blast")
+
 if __name__ == '__main__':
-    # Start Server
-    app.run(port=PORT)
+  cur = db.cursor()
+  # Create Database Tables on server launch
+  
+  # -- Input table
+  cur.execute("""
+    CREATE TABLE IF NOT EXISTS input (
+        id int PRIMARY KEY AUTO_INCREMENT,
+        sequence text NOT NULL
+    );
+  """)
+  
+  # -- Raw Blast table
+  cur.execute("""
+    CREATE TABLE IF NOT EXISTS raw_blast (
+        id int PRIMARY KEY AUTO_INCREMENT,
+        e_value text NOT NULL,
+        accession_code text NOT NULL,
+        protein_name text NOT NULL,
+        organism_name text NOT NULL
+    );
+  """)
+  
+  # -- Filtered Blast table
+  cur.execute("""
+    CREATE TABLE IF NOT EXISTS filtered_blast (
+        id int PRIMARY KEY AUTO_INCREMENT,
+        e_value text NOT NULL,
+        identity_perc int NOT NULL,
+        accession_code varchar(255) UNIQUE NOT NULL
+    );
+  """)
+  
+  # -- Protein table
+  cur.execute("""
+    CREATE TABLE IF NOT EXISTS protein (
+        id int PRIMARY KEY AUTO_INCREMENT,
+        protein_name text NOT NULL,
+        protein_function text NOT NULL,
+        hit_id int NOT NULL,
+        -- Constraints
+        CONSTRAINT prot_blast_hit
+        FOREIGN KEY (hit_id)
+        REFERENCES filtered_blast(id)
+    );
+  """)
+  
+  # -- Organism table
+  cur.execute("""
+    CREATE TABLE IF NOT EXISTS organism (
+        id int PRIMARY KEY AUTO_INCREMENT,
+        organism_name text NOT NULL,
+        family text NOT NULL,
+        sex text NOT NULL,
+        species text NOT NULL,
+        hit_id int NOT NULL,
+        -- Constraints
+        CONSTRAINT org_blast_hit
+        FOREIGN KEY (hit_id)
+        REFERENCES filtered_blast(id)
+    );
+  """)
+  
+  # Close connection after table creation
+  cur.close()
+  
+  # Start Server
+  app.run(debug=PORT=="3000", port=PORT)
 
