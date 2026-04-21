5728b6efc6f0245c10fbe7bf3bed350eab4be77e
blazehp <blazehunterhp@gmail.com>
2026-04-21 16:14:14 +0200

    Added dotenv capabilities and gunicorn to server

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 # https://flask.palletsprojects.com/en/stable/tutorial/layout/
 
 from flask import Flask, render_template, redirect
+import os
+from dotenv import load_dotenv
+
+# Load environment variables into the server
+# This is the .env when in development mode
+load_dotenv()
 
 app = Flask(__name__)
+
+DB_URL = os.getenv("DB_URL")
 
 @app.route('/')
 def index():
@@ -14,5 +22,5 @@ def blast():
 
 if __name__ == '__main__':
     # Start Server
-    app.run(debug=True, port=3000)
+    app.run()
 
