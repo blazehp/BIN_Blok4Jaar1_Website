741c91d7a837fe2f21a3e97d81893c09ec0ee3ab
Ruben Koker <ruben.koker@itvitaelearning.nl>
2026-05-05 14:38:47 +0200

    created parsing.py

---
+++ visualisation.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+
+def create_piechart(values: dict[str, int], title: str = "piechart"):
+
+    labels = values.keys()
+    fig, ax = plt.subplots()
+    ax.pie(values.values(), labels=labels)
+    plt.title(title)
+    plt.plot(values.keys(), values.values())
+    plt.savefig(f"graphs/{title}.svg")
+
+
+if __name__ == "__main__":
+    dict = {
+        "dingdong": 1,
+        "ding": 2,
+        "test": 3,
+        "qwerty": 3
+
+    }
+    create_piechart(dict)
