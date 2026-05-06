f13695da117805238f7bcf4389f59a5f132a5108
mguillorit <mmj.guillorit@student.han.nl>
2026-05-06 21:56:55 +0200

    first working version of a blastx query and parse function

--- BLAST.py
+++ BLAST.py
@@ -9,25 +9,20 @@ import requests
 from requests import Response
 import asyncio
 from asyncio import Task
+from Bio import Blast
 from Bio.Blast import NCBIXML, NCBIWWW
 from time import sleep
 from database_manager import DatabaseManager
 from database_manager import db
+import re
+import time
 
-NCBIWWW.email = "mmj.guillorit@sudent.han.nl"
+Blast.email = "mmj.guillorit@sudent.han.nl"
 
-async def fetch_status(url:str) -> dict:
-    print(f"Fetching status for: {url}")
-    response :Response = await asyncio.to_thread(requests.get,url,None)
-    return {'status': response.status_code, 'url':url}
 
-async def main():
-    apple_task: Task[dict] = asyncio.create_task(fetch_status("https://apple.com"))
-    google_task: Task[dict] = asyncio.create_task(fetch_status("https://google.com"))
-    apple_status : dict = await apple_task
-    google_status : dict = await google_task
-    print(apple_status)
-    print(google_status)
+def extract_organism(hit_def: str) -> str:
+    return hit_def
+
 
 
 
@@ -35,17 +30,30 @@ def query(sequence : str ):
     """
     :param sequence:
 
-    :return: return the data stream of an xml file to parse the needed information
+    :return: returns a string
     """
     print(f"Querying {sequence}")
-    results_handle = NCBIWWW.qblast(program="blastx",database="nr",sequence = sequence,matrix_name='BLOSUM62')
-    print('results retrieved')
-    return results_handle
-
-def query_parser(data_stream : 'str'):
+    results_handle = NCBIWWW.qblast(program="blastx",database="nr",sequence = sequence)
+    #results retrieved
+    blast_records = NCBIXML.parse(results_handle)
+    blast_record = next(blast_records)
+    print(blast_record)
+    for alignment in blast_record.alignments:
+        for hsp in alignment.hsps:
+            print(f"Hit Title: {alignment.title}")
+            print(f"Hit Score: {hsp.escore}")
+            print(f"Hit Type : {hsp.hit_type}")
+            print(f"Hit escore: {hsp.escore}")
     return
 
-def query_excel(data_frames : tuple ) -> tuple[str, str]:
+
+
+
+
+
+
+
+def query_excel(data_frames : tuple ) -> dict:
     """
     :param data_frames:
 
@@ -53,26 +61,26 @@ def query_excel(data_frames : tuple ) -> tuple[str, str]:
     """
     df_1 = data_frames[0]
     df_2 = data_frames[1]
-    for index, row in df_1.iterrows():
-        print(f"Querying {df_1.iloc[index, 0]}")
-        results_handle = NCBIWWW.qblast("blastx", "nt", df_1.iloc[index][1])
-
-    data = {
+    template = {
         "e_value": "",
         "accession_code": "",
         "protein_name": "",
         "organism_name": "",
     }
+    for index, row in (df_1.iterrows(), df_2.iterrows()):
+        print(f"Querying {df_1.iloc[index, 0]}")
+        results_handle = NCBIWWW.qblast("blastx", "nt", df_1.iloc[index][1])
+
     cur = db.cursor()
     dm = DatabaseManager(cur)
-    dm.insert("raw_blast", data)
+    dm.insert("raw_blast", template)
     return
 
 
 
 
 if __name__ == "__main__":
-    print(query('ACTGCTGATCGATCGATCGTAGCTAGCTAGCTAGCTAGCTAGCTAG'))
+    queried = query('ACATTTGCTTCTGACACAACTGTGTTCACTAGCAACCTCAAACAGACACCATGGTGCATCTGACTCCTGAGGAGAAGTCTGCCGTTACTGCCCTGTGGGGCAAGGTGAACGTGGATGAAGTTGGTGGTGAGGCCCTGGGCAGGCTGCTGGTGGTCTACCCTTGGACCCAGAGGTTCTTTGAGTCCTTTGGGGATCTGTCCACTCCTGATGCTGTTATGGGCAACCCTAAGGTGAAGGCTCATGGCAAGAAAGTGCTCGGTGCCTTTAGTGATGGCCTGGCTCACCTGGACAACCTCAAGGGCACCTTTGCCACACTGAGTGAGCTGCACTGTGACAAGCTGCACGTGGATCCTGAGAACTTCAGGCTCCTGGGCAACGTGCTGGTCTGTGTGCTGGCCCATCACTTTGGCAAAGAATTCACCCCACCAGTGCAGGCTGCCTATCAGAAAGTGGTGGCTGGTGTGGCTAATGCCCTGGCCCACAAGTATCACTAAGCTCGCTTTCTTGCTGTCCAATTTCTATTAAAGGTTCCTTTGTTCCCTAAGTCCAACTACTAAACTGGGGGATATTATGAAGGGCCTTGAGCATCTGGATTCTGCCTAATAAAAAACATTTATTTTCATTGCAA')
 
 ###
 """"
@@ -91,3 +99,19 @@ for alignment in blast_record.alignments:
         print(f"Alignment Title: {alignment.title}")
         print(f"Alignment Score: {hsp.score}")
 """
+
+
+"""
+async def fetch_status(url:str) -> dict:
+    print(f"Fetching status for: {url}")
+    response :Response = await asyncio.to_thread(requests.get,url,None)
+    return {'status': response.status_code, 'url':url}
+
+async def main():
+    apple_task: Task[dict] = asyncio.create_task(fetch_status("https://apple.com"))
+    google_task: Task[dict] = asyncio.create_task(fetch_status("https://google.com"))
+    apple_status : dict = await apple_task
+    google_status : dict = await google_task
+    print(apple_status)
+    print(google_status)
+    """
