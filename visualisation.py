af6c6c0b1aa1b17879a0bcdb1600c8aba1846731
Ruben Koker <ruben.koker@itvitaelearning.nl>
2026-06-02 15:12:32 +0200

    both graph functions plot to SVG files

--- visualisation.py
+++ visualisation.py
@@ -1,6 +1,5 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.pyplot import figure
 
 
 def create_piechart(values: dict, title: str):
@@ -8,13 +7,13 @@ def create_piechart(values: dict, title: str):
 
     plt.pie(values.values(), labels=keys)
     plt.title(title)
-
+    plt.savefig("graphs/" + title + ".svg")
     plt.show()
 
 
 def create_sunburst_chart(nodes: list[tuple],
                           title: str = "a Sunburst diagram(please insert a title next time)", level=0,
-                          total=np.pi * 2, ax=None, offset=0 ):
+                          total=np.pi * 2, ax=None, offset=0):
     ax = ax or plt.subplot(111, title=title, projection='polar')
 
     if level == 0 and len(nodes) == 1:
@@ -30,22 +29,23 @@ def create_sunburst_chart(nodes: list[tuple],
         for label, value, subnodes in nodes:
             labels.append(label)
             widths.append(value * diameter)
-            create_sunburst_chart(subnodes, total=total, title=title, ax=ax, level = level + 1,offset=local_offset )
+            create_sunburst_chart(subnodes, total=total, title=title, ax=ax, level=level + 1, offset=local_offset)
             local_offset += value
         values = np.cumsum([offset * diameter] + widths[:-1])
         heights = [1] * len(nodes)
         bottoms = np.zeros(len(nodes)) + level - 0.5
-        rects = ax.bar(values, heights, widths,bottoms,linewidth = 1, align='edge', edgecolor='black')
+        rects = ax.bar(values, heights, widths, bottoms, linewidth=1, align='edge', edgecolor='black')
         for rect, label in zip(rects, labels):
             x = rect.get_x() + rect.get_width() / 2
             y = rect.get_y() + rect.get_height() / 2
-            rotation = (90 + (360- np.degrees(x)% 180) )%360
+            rotation = (90 + (360 - np.degrees(x) % 180)) % 360
             ax.text(x, y, label, ha='center', va='center', rotation=rotation, size='small', color='yellow')
     if level == 0:
         ax.set_theta_direction(-1)
         ax.set_theta_zero_location('N')
         ax.set_axis_off()
 
+        plt.savefig("graphs/" + title + ".svg")
         plt.show()
 
 
@@ -57,7 +57,7 @@ if __name__ == '__main__':
         "boops boops": 8
 
     }
-    create_piechart(species_dict, title="testdata")
+    create_piechart(species_dict, title="barplot")
     data = [
         ('eukarya', 100, [
             ('fungi', 70, [
