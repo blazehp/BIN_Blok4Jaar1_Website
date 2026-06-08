bffc0711390e6f7875e17bb83ebaae04d5db6e58
Ruben Koker <ruben.koker@itvitaelearning.nl>
2026-06-08 17:27:00 +0200

    SQL art of the  all_species_graph function in analysis.py added

--- analysis.py
+++ analysis.py
@@ -1,12 +1,18 @@
 import visualisation as vs
+from database_config import db
+from database_manager import RawBlastTable
 
 
-# def all_species_graph():
+def all_species_graph():
+    cur = db.cursor()
+    flblast_db = RawBlastTable(cur)
+    result = flblast_db.custom_query("""SELECT organism_name, count(organism_name)
+                                   FROM railway.raw_blast t
+                                   group by organism_name
+                                   order by count(organism_name) desc LIMIT 501
+                                """)
 
 
-# cur = db.cursor()
-# dm = DatabaseManager(cur)
-#  cur.execute("SELECT * FROM input")
 # dummy function for the analysis page please
 def species_graph():
     data = [
@@ -31,3 +37,8 @@ def species_graph():
         ]),
     ]
     vs.create_sunburst_chart(data, title="Speciestest")
+
+
+if __name__ == '__main__':
+    species_graph()
+    all_species_graph()
